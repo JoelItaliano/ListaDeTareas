ad13f9b9ae237b63816d2763fb1a2830de8b325d
Joel Italiano <92814212+JoelItaliano@users.noreply.github.com>
2022-11-07 18:49:08 -0300

    clase 03 completada

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const btnAgregar = document.querySelector("[data-form-btn]");
 
 
-const createTask = ((evento) => {
-    evento.preventDefault; //previene el duncionamiento por defecto del formulario (refescar pag)
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value)
-})
+const createTask = (evento) => {
+    evento.preventDefault(); //previene el duncionamiento por defecto del formulario (refescar pag)
+    const input = document.querySelector('[data-form-input]');
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li');
+         
+    task.classList.add('card');
 
-btnAgregar.addEventListener(createTask)
+    const contenido = agregaHTML(input.value);
+    input.value = '';
+
+    task.innerHTML = contenido; //agrega contenido a html
+    list.appendChild(task) 
+}
+
+btnAgregar.addEventListener('click', createTask);
+
+function agregaHTML(valor) {
+   return `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${valor}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+}
